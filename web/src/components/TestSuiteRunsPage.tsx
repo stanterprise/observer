da5baa00c8014f8a448a5d1cb2d2d2d9e59fd9d7
Stanislav Fedii <stanislav.fedii@gmail.com>
2025-12-03 21:25:57 -0600

    Remove unused CardHeader and CardTitle imports from TestSuiteRunsPage component

--- web/src/components/TestSuiteRunsPage.tsx
+++ web/src/components/TestSuiteRunsPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { apiUrl } from "../lib/config";
-import { Card, CardHeader, CardTitle, CardContent } from "./Card";
+import { Card, CardContent } from "./Card";
 import { Badge } from "./Badge";
 import type {
   WebSocketEvent,
