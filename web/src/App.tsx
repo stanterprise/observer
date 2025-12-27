774a05e236b2ec47c6992cbc810bc44533a37307
Stanislav Fedii <stanislav.fedii@gmail.com>
2025-12-27 00:40:35 -0600

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 
 import {
-  TestRunsPage,
   TestRunDetailPage,
   TestDetailPage,
   TestSuiteRunsPage,
